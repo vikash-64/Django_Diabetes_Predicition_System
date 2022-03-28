0438ec1697a024a5c203e3f34c3ea0b0249b5d63
Vikash Kr Upadhyay <vikash400001@gmail.com>
2022-03-28 12:06:43 +0530

    first_commit

--- diabites/settings.py
+++ diabites/settings.py
@@ -15,7 +15,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-zbzvfr$f+58iq8-5*=7xk9p^^!k_24sa21r8e-^at#7b55^(47'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
